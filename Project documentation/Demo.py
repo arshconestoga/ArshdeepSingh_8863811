f500ab2713d5fa3e0790601d3fe3c662b869743a
arshconestoga <arshmakkar@hotmail.com>
2023-02-11 21:15:57 -0500

    Demo.py file updated

--- Project documentation/Demo.py
+++ Project documentation/Demo.py
@@ -8,7 +8,8 @@ class TestPrime(unittest.TestCase):
     	self.assertTrue(is_prime(5))
     def test_nine(self):
     	self.assertFalse(is_prime(9))
-    def test_eleven(self):
+    def test_eleven(yourself):
     	self.assertTrue(is_prime(11))
 if __name__=='__main__':
 	unittest.main()
+
